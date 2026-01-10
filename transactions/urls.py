7c5fb6cf59acf5b3dc287b2393915e6bf9798304
nzommmo <ericnzomo17@gmail.com>
2026-01-10 14:00:06 +0300

    Endpoint to generate reports

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from transactions.views import AllocateFundsView, TopUpView
+from transactions.views import AllocateFundsView, TopUpView, ReportsView
 
 urlpatterns = [
-    path("allocate/", AllocateFundsView.as_view(), name="allocate-funds"),
-     path("top-up/", TopUpView.as_view()),
+    path("allocate/", AllocateFundsView.as_view()),
+    path("top-up/", TopUpView.as_view()),
+    path("reports/", ReportsView.as_view()),  
 ]
